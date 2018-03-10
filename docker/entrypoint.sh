9e6a7a8bfe8e78ec8ff0e16ff24ebdbd78a8d7f5
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2018-03-10 19:04:32 +0100

    [db_importer] Enhance dockerization

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-diesel setup --database-url=$1 --migration-dir=db_persistence/migrations && db_importer --database-url=$1
+
+diesel setup --database-url=$1 --migration-dir=db_persistence/migrations && db_importer /blk_files --database-url=$1
