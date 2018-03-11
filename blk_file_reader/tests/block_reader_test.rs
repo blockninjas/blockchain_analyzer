70b1fc480fe329eb13f90e5a107f87abf1e979b0
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2018-03-11 15:34:01 +0100

    [blk_file_reader] Remove redundant String::from() conversions

--- blk_file_reader/tests/block_reader_test.rs
+++ blk_file_reader/tests/block_reader_test.rs
@@ -18,15 +18,15 @@ pub fn can_read_genesis_block() {
   assert_eq!(block.version, 1);
   assert_eq!(
     block.hash.to_hex_string().to_lowercase(),
-    String::from("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f")
+    "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
   );
   assert_eq!(
     block.previous_block_hash.to_hex_string().to_lowercase(),
-    String::from("0000000000000000000000000000000000000000000000000000000000000000")
+    "0000000000000000000000000000000000000000000000000000000000000000"
   );
   assert_eq!(
     block.merkle_root.to_hex_string().to_lowercase(),
-    String::from("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b")
+    "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
   );
   assert_eq!(block.creation_time, 1231006505);
   assert_eq!(block.bits, 486604799);
