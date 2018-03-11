374c6245d89ba499fcaa47d616455e6ec5df3219
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2018-03-11 17:58:37 +0100

    [blk_file_reader] Enhance genesis block test
    Add checks to ensure the genesis block's transaction, input, output and
    address are read correct.y

--- blk_file_reader/tests/block_reader_test.rs
+++ blk_file_reader/tests/block_reader_test.rs
@@ -8,6 +8,7 @@ use blk_file_reader::BlockReader;
 
 const PATH_TO_BLK_FILE_0: &'static str = "sample_blk_files/blk00000.dat";
 
+// TODO Split up into multiple tests to improve readability.
 #[test]
 pub fn can_read_genesis_block() {
   // given
@@ -32,4 +33,31 @@ pub fn can_read_genesis_block() {
   assert_eq!(block.bits, 486604799);
   assert_eq!(block.nonce, 2083236893);
   assert_eq!(block.transactions.len(), 1);
+  assert_eq!(
+    block.transactions[0].tx_hash.to_hex_string().to_lowercase(),
+    "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
+  );
+  assert_eq!(block.transactions[0].version, 1);
+  assert_eq!(block.transactions[0].lock_time, 0);
+  assert_eq!(block.transactions[0].inputs.len(), 1);
+  assert_eq!(block.transactions[0].inputs[0].sequence_number, 4294967295);
+  assert_eq!(
+    block.transactions[0].inputs[0]
+      .previous_tx_hash
+      .to_hex_string()
+      .to_lowercase(),
+    "0000000000000000000000000000000000000000000000000000000000000000"
+  );
+  assert_eq!(
+    block.transactions[0].inputs[0].previous_tx_output_index,
+    4294967295
+  );
+  assert_eq!(block.transactions[0].outputs.len(), 1);
+  assert_eq!(block.transactions[0].outputs[0].index, 0);
+  assert_eq!(block.transactions[0].outputs[0].value, 5_000_000_000);
+  assert_eq!(block.transactions[0].outputs[0].addresses.len(), 1);
+  assert_eq!(
+    block.transactions[0].outputs[0].addresses[0].base58_string,
+    "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa"
+  );
 }
