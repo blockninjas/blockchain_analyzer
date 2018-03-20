47ee97619ade1347a9b498fd92c01da2f9de8415
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2018-03-20 08:58:57 +0100

    [db_importer] Add build/deployment script for docker

---
+++ deploy_db_importer.sh
@@ -0,0 +1,5 @@
+docker build -t bitcoin/db_importer -f Dockerfile.db_importer .
+
+$(aws ecr get-login --no-include-email --region eu-west-1) && \
+  docker tag bitcoin/db_importer:latest 554480245419.dkr.ecr.eu-west-1.amazonaws.com/bitcoin/db_importer:latest && \
+  docker push 554480245419.dkr.ecr.eu-west-1.amazonaws.com/bitcoin/db_importer:latest
