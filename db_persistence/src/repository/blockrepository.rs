9d42562197c99b8b83c1b150086e4d4f3448f92a
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2018-03-25 21:49:16 +0200

    [db_importer] Allow for retrieving saved blocks

--- db_persistence/src/repository/blockrepository.rs
+++ db_persistence/src/repository/blockrepository.rs
@@ -2,9 +2,11 @@ use super::Repository;
 use domain::Block;
 use domain::NewBlock;
 use schema::blocks;
+use schema::blocks::dsl::*;
 use diesel;
 use diesel::pg::PgConnection;
 use diesel::RunQueryDsl;
+use diesel::QueryDsl;
 
 pub struct BlockRepository<'a> {
   connection: &'a PgConnection,
@@ -13,6 +15,16 @@ pub struct BlockRepository<'a> {
 impl<'a> BlockRepository<'a> {
   pub fn new(connection: &'a PgConnection) -> BlockRepository<'a> {
     BlockRepository { connection }
+  }
+
+  pub fn count(&self) -> i64 {
+    // TODO Return error instead of panicking.
+    blocks.count().get_result(self.connection).unwrap()
+  }
+
+  pub fn read_all(&self) -> Vec<Block> {
+    // TODO Return error instead of panicking.
+    blocks.load::<Block>(self.connection).unwrap()
   }
 }
 
