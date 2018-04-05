00af2e97c02456d2c3598aa7fe3bac5e66ec97f1
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2018-04-05 22:10:13 +0200

    [db_importer] Change docker mount point for blk file directory

--- docker/db_importer/entrypoint.sh
+++ docker/db_importer/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# From where the `db_importer` reads blk files.
-BLK_FILE_ROOT=~/.bitcoin/blocks
+# From where to read the blk files to import into the database.
+BLK_FILE_DIR=/home/blockninjas/blk_files
 
-diesel setup --database-url=$1 --migration-dir=/analysis_suite/db_persistence/migrations && db_importer --database-url=$1 $BLK_FILE_ROOT
+diesel setup --database-url=$1 --migration-dir=/analysis_suite/db_persistence/migrations && db_importer --database-url=$1 $BLK_FILE_DIR
