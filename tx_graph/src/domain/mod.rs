e63bc31637ac850b5b8daf96f034f7ca6d498827
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2018-04-05 22:06:54 +0200

    [tx_graph] Add left-overs from previous commit

--- tx_graph/src/domain/mod.rs
+++ tx_graph/src/domain/mod.rs
@@ -1,17 +1,13 @@
 mod transaction;
 mod new_transaction;
-mod transaction_hash;
 mod input;
 mod new_input;
 mod output;
 mod new_output;
-mod address;
 
 pub use self::transaction::Transaction;
 pub use self::new_transaction::NewTransaction;
-pub use self::transaction_hash::TransactionHash;
 pub use self::input::Input;
 pub use self::new_input::NewInput;
 pub use self::output::Output;
 pub use self::new_output::NewOutput;
-pub use self::address::Address;
--- tx_graph/src/repository/mod.rs
+++ tx_graph/src/repository/mod.rs
@@ -1,9 +1,5 @@
 mod transaction_repository;
 mod mmap_transaction_repository;
-mod transaction_hash_repository;
-mod address_repository;
 
 pub use self::transaction_repository::TransactionRepository;
 pub use self::mmap_transaction_repository::MmapTransactionRepository;
-pub use self::transaction_hash_repository::TransactionHashRepository;
-pub use self::address_repository::AddressRepository;
