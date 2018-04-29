898b33df51a24ac75ce28b552a90b29eca608830
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2018-04-29 17:29:31 +0200

    [tx_graph] Introduce value and source_address_id fields in Input

--- tx_graph/src/read/input.rs
+++ tx_graph/src/read/input.rs
@@ -1,21 +1,6 @@
 pub struct Input {
-  spent_transaction_id: u64,
-  spent_output_index: u32,
-}
-
-impl Input {
-  pub fn new(spent_transaction_id: u64, spent_output_index: u32) -> Input {
-    Input {
-      spent_transaction_id,
-      spent_output_index,
-    }
-  }
-
-  pub fn get_spent_transaction_id(&self) -> u64 {
-    self.spent_transaction_id
-  }
-
-  pub fn get_spent_output_index(&self) -> u32 {
-    self.spent_output_index
-  }
+  pub spent_transaction_id: u64,
+  pub spent_output_index: u32,
+  pub value: u64,
+  pub source_address_id: u64,
 }
--- tx_graph/src/read/read_input.rs
+++ tx_graph/src/read/read_input.rs
@@ -10,7 +10,14 @@ impl<'a> ReadInput for Cursor<&'a [u8]> {
   fn read_input(&mut self) -> Result<Input> {
     let spent_transaction_id = self.read_u64::<LittleEndian>()?;
     let spent_output_index = self.read_u32::<LittleEndian>()?;
-    let input = Input::new(spent_transaction_id, spent_output_index);
+    let value = self.read_u64::<LittleEndian>()?;
+    let source_address_id = self.read_u64::<LittleEndian>()?;
+    let input = Input {
+      spent_transaction_id,
+      spent_output_index,
+      value,
+      source_address_id,
+    };
     Ok(input)
   }
 }
