f11c882097f625940f1dea8dd0f65842e9912f7b
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2018-06-09 20:50:57 +0200

    [db_importer] Perform address deduplication within a transaction

--- db_importer/src/address_deduplication_task.rs
+++ db_importer/src/address_deduplication_task.rs
@@ -14,27 +14,29 @@ impl AddressDeduplicationTask {
 impl Task for AddressDeduplicationTask {
   fn run(&self, _config: &Config, db_connection: &PgConnection) {
     info!("Deduplicate addresses");
+    db_connection
+      .transaction::<(), diesel::result::Error, _>(|| {
+        let address_deduplicator_state_repository =
+          AddressDeduplicatorStateRepository::new(db_connection);
 
-    let address_deduplicator_state_repository =
-      AddressDeduplicatorStateRepository::new(db_connection);
+        let latest_deduplicated_output_address_id =
+          match address_deduplicator_state_repository.latest() {
+            Some(id) => id,
+            None => 0,
+          };
 
-    let latest_deduplicated_output_address_id =
-      match address_deduplicator_state_repository.latest() {
-        Some(id) => id,
-        None => 0,
-      };
+        let address_repository = AddressRepository::new(db_connection);
+        address_repository
+          .deduplicate_output_addresses(latest_deduplicated_output_address_id);
 
-    let address_repository = AddressRepository::new(db_connection);
-    address_repository
-      .deduplicate_output_addresses(latest_deduplicated_output_address_id);
+        let output_address_repository =
+          OutputAddressRepository::new(db_connection);
+        let max_output_address_id = output_address_repository.max_id().unwrap();
 
-    let output_address_repository = OutputAddressRepository::new(db_connection);
-    let max_output_address_id = output_address_repository.max_id().unwrap();
-
-    address_deduplicator_state_repository.save(max_output_address_id);
-
-    diesel::sql_query(format!("CREATE UNIQUE INDEX IF NOT EXISTS addresses_base58check_index ON addresses ( base58check )")).execute(db_connection)
-    .unwrap();
+        address_deduplicator_state_repository.save(max_output_address_id);
+        Ok(())
+      })
+      .unwrap();
   }
 
   fn get_indexes(&self) -> Vec<Index> {
