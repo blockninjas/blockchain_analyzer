54630330538ef889971977b99d0179aff1146b6e
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2018-07-18 19:21:57 +0200

    [lru_cached_address_map] Record cache hits & misses

--- blockchain_analyzer/src/tasks/bir_resolver/lru_cached_address_map.rs
+++ blockchain_analyzer/src/tasks/bir_resolver/lru_cached_address_map.rs
@@ -1,10 +1,12 @@
 use super::{address_map::Address, address_map::AddressId, AddressMap};
 use lru_cache::LruCache;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub struct LruCachedAddressMap<A: AddressMap> {
   lru_addresses: LruCache<String, AddressId>,
   address_map: A,
+  cache_hits: usize,
+  cache_misses: usize,
 }
 
 impl<A: AddressMap> LruCachedAddressMap<A> {
@@ -12,15 +14,27 @@ impl<A: AddressMap> LruCachedAddressMap<A> {
     LruCachedAddressMap {
       lru_addresses: LruCache::new(cache_size),
       address_map,
+      cache_hits: 0,
+      cache_misses: 0,
     }
+  }
+
+  pub fn get_cache_misses(&self) -> usize {
+    self.cache_misses
+  }
+
+  pub fn get_cache_hits(&self) -> usize {
+    self.cache_hits
   }
 }
 
 impl<A: AddressMap> AddressMap for LruCachedAddressMap<A> {
   fn get_id(&mut self, address: Address) -> AddressId {
     if let Some(&mut address_id) = self.lru_addresses.get_mut(address) {
+      self.cache_hits += 1;
       address_id
     } else {
+      self.cache_misses += 1;
       let address_id = self.address_map.get_id(address);
       self.lru_addresses.insert(String::from(address), address_id);
       address_id
@@ -28,12 +42,14 @@ impl<A: AddressMap> AddressMap for LruCachedAddressMap<A> {
   }
 
   fn get_ids(&mut self, addresses: &[String]) -> HashMap<String, AddressId> {
-    let mut cached_addresses: Vec<(String, AddressId)> = vec![];
-    let mut addresses_to_load: Vec<String> = vec![];
+    let addresses: HashSet<String> = addresses.into_iter().cloned().collect();
 
-    for address in addresses {
+    let mut cached_addresses = HashMap::<String, AddressId>::new();
+    let mut addresses_to_load = Vec::<String>::new();
+
+    for address in addresses.iter() {
       if let Some(&mut address_id) = self.lru_addresses.get_mut(address) {
-        cached_addresses.push((address.clone(), address_id));
+        cached_addresses.insert(address.clone(), address_id);
       } else {
         addresses_to_load.push(address.clone());
       }
@@ -44,6 +60,9 @@ impl<A: AddressMap> AddressMap for LruCachedAddressMap<A> {
     for (address, &address_id) in address_ids.iter() {
       self.lru_addresses.insert(address.clone(), address_id);
     }
+
+    self.cache_hits += cached_addresses.len();
+    self.cache_misses += address_ids.len();
 
     address_ids.extend(cached_addresses);
     address_ids
