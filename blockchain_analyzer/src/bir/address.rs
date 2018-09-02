09f204a0eccbe43f770f72eb652c4f9ef9404bf4
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2018-09-02 20:02:39 +0200

    [bir] Add convenience methods

--- blockchain_analyzer/src/bir/address.rs
+++ blockchain_analyzer/src/bir/address.rs
@@ -6,3 +6,13 @@ pub enum Address {
     Id(AddressId),
     Base58Check(String),
 }
+
+impl Address {
+    pub fn to_address_id(&self) -> Option<AddressId> {
+        if let Address::Id(address_id) = *self {
+            Some(address_id)
+        } else {
+            None
+        }
+    }
+}
--- blockchain_analyzer/src/bir/transaction.rs
+++ blockchain_analyzer/src/bir/transaction.rs
@@ -5,3 +5,19 @@ pub struct Transaction {
     pub inputs: Vec<Input>,
     pub outputs: Vec<Output>,
 }
+
+impl Transaction {
+    pub fn get_input_address_ids(&self) -> Vec<u64> {
+        self.inputs
+            .iter()
+            .filter_map(|input| input.address.to_address_id())
+            .collect()
+    }
+
+    pub fn get_output_address_ids(&self) -> Vec<u64> {
+        self.outputs
+            .iter()
+            .filter_map(|output| output.address.to_address_id())
+            .collect()
+    }
+}
