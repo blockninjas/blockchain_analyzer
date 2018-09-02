93dc5b102a29bc9a30b9b04fdc1c07aab742728e
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2018-09-02 20:04:00 +0200

    [clustering] Fix OtcHeuristic

--- blockchain_analyzer/src/tasks/clustering/heuristics/otc_heuristic.rs
+++ blockchain_analyzer/src/tasks/clustering/heuristics/otc_heuristic.rs
@@ -1,16 +1,9 @@
 use super::{Cluster, Heuristic};
-use bir::{Address, AddressId, Transaction};
+use bir::{AddressId, Transaction};
 use bit_vec::BitVec;
+use std::collections::HashSet;
 
-#[allow(dead_code)]
 pub struct OtcHeuristic {}
-
-impl OtcHeuristic {
-    #[allow(dead_code)]
-    pub fn new() -> OtcHeuristic {
-        OtcHeuristic {}
-    }
-}
 
 impl Heuristic for OtcHeuristic {
     fn cluster_addresses(
@@ -18,78 +11,44 @@ impl Heuristic for OtcHeuristic {
         used_addresses: &BitVec<u32>,
         transaction: &Transaction,
     ) -> Vec<Cluster> {
-        let mut clusters = vec![];
-        if let Some(otc_transaction) = to_otc_transaction(used_addresses, transaction) {
-            // "One-time change" heuristic.
-            let mut sender_cluster = otc_transaction.input_addresses;
-            sender_cluster.push(otc_transaction.change_address);
-            let receiver_cluster = vec![otc_transaction.receiver_address];
-            clusters.push(sender_cluster);
-            clusters.push(receiver_cluster);
+        if let Some(change_address) = get_one_time_change_address(transaction, used_addresses) {
+            let mut cluster = transaction.get_input_address_ids();
+            cluster.push(change_address);
+            vec![cluster]
+        } else {
+            vec![]
         }
-        clusters
     }
 }
 
-/// A one-time change transaction.
-struct OtcTransaction {
-    pub input_addresses: Vec<AddressId>,
-    pub receiver_address: AddressId,
-    pub change_address: AddressId,
-}
-
-/// Transforms the given transaction to an OtcTransaction.
-///
-/// Returns `Some` if the given `transaction` is an `OtcTransaction` in the
-/// current context, `None` otherwise.
-fn to_otc_transaction(
-    used_addresses: &BitVec<u32>,
+fn get_one_time_change_address(
     transaction: &Transaction,
-) -> Option<OtcTransaction> {
-    if transaction.outputs.len() != 2 {
-        None
-    } else {
-        let address0 = match transaction.outputs[0].address {
-            Address::Id(address_id) => address_id,
-            _ => return None,
-        };
-
-        let address1 = match transaction.outputs[1].address {
-            Address::Id(address_id) => address_id,
-            _ => return None,
-        };
-
-        let (change_address, receiver_address): (AddressId, AddressId) =
-            if is_change_address(used_addresses, address0)
-                && !is_change_address(used_addresses, address1)
-            {
-                (address0, address1)
-            } else if !is_change_address(used_addresses, address0)
-                && is_change_address(used_addresses, address1)
-            {
-                (address1, address0)
-            } else {
-                return None;
-            };
-
-        let otc_transaction = OtcTransaction {
-            input_addresses: transaction
-                .inputs
-                .iter()
-                .filter_map(|input| {
-                    if let Address::Id(address_id) = input.address {
-                        Some(address_id)
-                    } else {
-                        None
-                    }
-                })
-                .collect(),
-            receiver_address,
-            change_address,
-        };
-
-        Some(otc_transaction)
+    used_addresses: &BitVec<u32>,
+) -> Option<AddressId> {
+    if contains_self_change_address(transaction) {
+        return None;
     }
+
+    let one_time_output_addresses: Vec<u64> = transaction
+        .get_output_address_ids()
+        .into_iter()
+        .filter(|address_id| is_change_address(used_addresses, *address_id))
+        .collect();
+
+    if one_time_output_addresses.len() == 1 {
+        Some(one_time_output_addresses[0])
+    } else {
+        None
+    }
+}
+
+fn contains_self_change_address(transaction: &Transaction) -> bool {
+    let input_address_ids: HashSet<u64> = transaction.get_input_address_ids().into_iter().collect();
+
+    transaction
+        .get_output_address_ids()
+        .iter()
+        .any(|address_id| input_address_ids.contains(address_id))
 }
 
 /// Returns `true` if the given `address` is a change-address in the given
