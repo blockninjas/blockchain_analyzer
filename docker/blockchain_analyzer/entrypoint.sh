bc6376de49e88ceb285650c3f7798d31a028986d
Matthias Reisinger <matthias.j.reisinger@gmail.com>
2018-11-03 14:28:38 +0100

    Remove obsolete configuration key

--- docker/blockchain_analyzer/entrypoint.sh
+++ docker/blockchain_analyzer/entrypoint.sh
@@ -2,6 +2,5 @@
 
 echo DATABASE_URL=$DATABASE_URL
 echo BLK_FILE_PATH=$BLK_FILE_PATH
-echo ADDRESS_CACHE_SIZE=$ADDRESS_CACHE_SIZE
 
 diesel setup --database-url=${DATABASE_URL} --migration-dir=/analysis_suite/db_persistence/migrations && blockchain_analyzer
